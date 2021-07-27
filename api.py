04fe2055714db492744107846d517a15c8c866b9
Giang <giangb1609816@student.ctu.edu.vn>
2021-07-27 13:43:58 +0700

    Sửa file endpoints và api

--- api.py
+++ api.py
@@ -16,38 +16,48 @@ def send_message(id_sender, message):
         _respone = json.loads(_request.text)
         reply_url = ''
         # Dịch vụ FiberVNN
-        if ('#fiber' in _respone_bot[0]['text']):
-            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + sender_id + '&ma_dichvu=fiber'
-            print(_respone_bot[0]['text'].replace('#fiber', reply_url))
+        if ('#fiber' in _respone[0]['text']):
+            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + \
+                sender_id + '&ma_dichvu=fiber'
+            print(_respone[0]['text'].replace('#fiber', reply_url))
         # Dịch vụ MyTV
-        elif ('#mytv' in _respone_bot[0]['text']):
-            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + sender_id + '&ma_dichvu=mytv'
-            print(_respone_bot[0]['text'].replace('#mytv', reply_url))
+        elif ('#mytv' in _respone[0]['text']):
+            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + \
+                sender_id + '&ma_dichvu=mytv'
+            print(_respone[0]['text'].replace('#mytv', reply_url))
         # Dịch vụ di động
-        elif ('#didong' in _respone_bot[0]['text']):
-            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + sender_id + '&ma_dichvu=didong'
-            print(_respone_bot[0]['text'].replace('#didong', reply_url))
+        elif ('#didong' in _respone[0]['text']):
+            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + \
+                sender_id + '&ma_dichvu=didong'
+            print(_respone[0]['text'].replace('#didong', reply_url))
         # Dịch vụ chữ ký số
-        elif ('#chukyso' in _respone_bot[0]['text']):
-            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + sender_id + '&ma_dichvu=chukyso'
-            print(_respone_bot[0]['text'].replace('#chukyso', reply_url))
+        elif ('#chukyso' in _respone[0]['text']):
+            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + \
+                sender_id + '&ma_dichvu=chukyso'
+            print(_respone[0]['text'].replace('#chukyso', reply_url))
         # Dịch vụ hóa đơn điện tử
-        elif ('#hddt' in _respone_bot[0]['text']):
-            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + sender_id + '&ma_dichvu=hddt'
-            print(_respone_bot[0]['text'].replace('#hddt', reply_url))
+        elif ('#hddt' in _respone[0]['text']):
+            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + \
+                sender_id + '&ma_dichvu=hddt'
+            print(_respone[0]['text'].replace('#hddt', reply_url))
         # Dịch vụ pharmacy
-        elif ('#pharmacy' in _respone_bot[0]['text']):
-            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + sender_id + '&ma_dichvu=pharmacy'
-            print(_respone_bot[0]['text'].replace('#pharmacy', reply_url))
+        elif ('#pharmacy' in _respone[0]['text']):
+            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + \
+                sender_id + '&ma_dichvu=pharmacy'
+            print(_respone[0]['text'].replace('#pharmacy', reply_url))
         # Dịch vụ Bảo hiểm xã hội
-        elif ('#bhxh' in _respone_bot[0]['text']):
-            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + sender_id + '&ma_dichvu=bhxh'
-            print(_respone_bot[0]['text'].replace('#bhxh', reply_url))
+        elif ('#bhxh' in _respone[0]['text']):
+            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + \
+                sender_id + '&ma_dichvu=bhxh'
+            print(_respone[0]['text'].replace('#bhxh', reply_url))
         # Dịch vụ khác
-        elif ('#khac' in _respone_bot[0]['text']):
-            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + sender_id + '&ma_dichvu=khac'
-            print(_respone_bot[0]['text'].replace('#khac', reply_url))
-    except:
+        elif ('#khac' in _respone[0]['text']):
+            reply_url = 'https://vnptvinhlong.vn/donhang/tao?zaloid=' + \
+                sender_id + '&ma_dichvu=khac'
+            print(_respone[0]['text'].replace('#khac', reply_url))
+        else:
+            print(_respone[0]['text'])
+    except ConnectionError:
         print("Connection Error!")
 
 
