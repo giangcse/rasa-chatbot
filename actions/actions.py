d48c25eaa91be2356b4e2c3c8a15b8dc16f2273c
Giang <giangb1609816@student.ctu.edu.vn>
2021-07-28 10:26:19 +0700

    Thêm các hastag hỗ trợ để chuyển trực tiếp đến nhân viên hỗ trợ.

--- actions/actions.py
+++ actions/actions.py
@@ -6,22 +6,41 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+import json
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class Action_chaohoi(Action):
+
+    def name(self) -> Text:
+        return "action_chaohoi"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        button = {
+            "type": "phone_number",
+            "title": "Call hotline",
+            "payload": "1900588856"
+        }
+
+        button1 = {
+            "type": "web_url",
+            "url": "https://miai.vn",
+            "title": "Website"
+        }
+        button2 = {
+            "type": "postback",
+            "title": "Need more?",
+            "payload": "more"
+        }
+        ret_text = "Hi! You can contact us via below methods:"
+        dispatcher.utter_message(text=ret_text, buttons=[
+                                 button, button1, button2])
+
+        return []
--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ import os
 import threading
 
 if __name__ == '__main__':
-    threading.Thread(target=os.system, args=("rasa run --enable-api", )).start()
+    # threading.Thread(target=os.system, args=("rasa run --enable-api", )).start()
     app.run(host='0.0.0.0', port='5000', debug=True)
